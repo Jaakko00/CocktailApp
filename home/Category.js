6f8bed33de6f65a9ac89bdcd340b1224538dda81
Jaakko Saranpää <jaakko.saranpaa@tuni.fi>
2022-01-25 19:18:51 +0200

    Add multiple categories to home

--- home/Category.js
+++ home/Category.js
@@ -1,8 +1,33 @@
 import react, { useEffect, useState } from "react";
 import { StyleSheet, View, ScrollView, Text } from "react-native";
 import DrinkSquare from "./DrinkSquare";
+import axios from "axios";
 
 export default function Category(props) {
+  const [search, setSearch] = useState();
+  const [searchResult, setSearchResult] = useState([{}]);
+
+  useEffect(() => {
+    if (search == null) {
+      searchCategory(props.category);
+    }
+  });
+
+  let searchCategory = (category) => {
+    axios
+      .request(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
+      )
+      .then(function (response) {
+        if (response.data !== null) {
+          setSearchResult(response.data.drinks);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
   let results;
   let noResults = (
     <View style={styles.results}>
@@ -11,8 +36,8 @@ export default function Category(props) {
       </Text>
     </View>
   );
-  if (props.searchResult !== null) {
-    results = props.searchResult.map(
+  if (searchResult !== null) {
+    results = searchResult.map(
       ({ strDrink, idDrink, strCategory, strAlcoholic, strDrinkThumb }) => (
         <DrinkSquare
           key={idDrink}
@@ -27,8 +52,8 @@ export default function Category(props) {
 
   return (
     <View>
-      <ScrollView
-      horizontal>
+      <Text>{props.category}</Text>
+      <ScrollView horizontal>
         {results}
         {results == null && noResults}
       </ScrollView>
--- home/Home.js
+++ home/Home.js
@@ -13,54 +13,20 @@ import SearchResults from "../search/SearchResults";
 import Category from "./Category";
 
 export default function Home() {
-  const [search, setSearch] = useState();
-  const [searchResult, setSearchResult] = useState([{}]);
-
-  useEffect(() => {
-    if (search == null) {
-      searchCocktail("");
-    }
-  });
-
-  let searchCocktail = (e) => {
-    console.log(e);
-    axios
-      .request(
-        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=shot`
-      )
-      .then(function (response) {
-        if (response.data !== null) {
-          setSearchResult(response.data.drinks);
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
-
-  
-
   return (
     <View style={styles.container}>
-      <View style={styles.search}>
-        
-      </View>
-      <Category searchResult={searchResult}></Category>
+      <ScrollView>
+        <Category category="Shot"></Category>
+        <Category category="Cocktail"></Category>
+        <Category category="Punch / Party Drink"></Category>
+        <Category category="Punch / Party Drink"></Category>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
-  search: {
-    marginBottom: 0,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    borderWidth: 0,
-  },
-
   input: {
     flex: 1,
     height: 50,
