935e8e3f580c05756c17f8690eda07694076fca5
Jaakko Saranpää <jaakko.saranpaa@tuni.fi>
2022-01-25 18:52:28 +0200

    Initialize home.js

--- Home.js
+++ Home.js
@@ -3,20 +3,68 @@ import {
   StyleSheet,
   Text,
   View,
+  TextInput,
   Button,
-  Image,
-  ImageBackground,
+  ScrollView,
 } from "react-native";
+import axios from "axios";
+import DrinkCard from "./DrinkCard";
+import SearchResults from "./SearchResults";
 
+export default function Home() {
+  const [search, setSearch] = useState();
+  const [searchResult, setSearchResult] = useState([{}]);
 
-export default function Home(props) {
+  useEffect(() => {
+    if (search == null) {
+      searchCocktail("");
+    }
+  });
+
+  let searchCocktail = (e) => {
+    console.log(e);
+    axios
+      .request(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=shot`
+      )
+      .then(function (response) {
+        if (response.data !== null) {
+          setSearchResult(response.data.drinks);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.search}>
+        
+      </View>
+      <SearchResults searchResult={searchResult}></SearchResults>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    
+  container: {},
+  search: {
+    marginBottom: 0,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    borderWidth: 0,
+  },
+
+  input: {
+    flex: 1,
+    height: 50,
+    borderWidth: 0,
+    padding: 10,
+    fontSize: 20,
+  },
 });
